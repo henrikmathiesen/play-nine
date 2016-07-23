c56d251d16521c2a335c56feb66c51cf2bc16e51
Henrik <henrik_mathiesen@hotmail.com>
2016-07-23 18:29:44 +0200

    -desing

---
+++ src/components/numbersFrame/NumbersFrame.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './numbers-frame.css';
+
+class NumbersFrame extends Component { 
+    render(){
+        return(
+            <div id="numbers-frame" className="well well-lg">
+                NumbersFrame
+            </div>
+        );
+    }
+}
+
+export default NumbersFrame;
--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import StarsFrame from '../../components/starsFrame/StarsFrame';
 import ButtonFrame from '../../components/buttonFrame/ButtonFrame';
 import AnswerFrame from '../../components/answerFrame/AnswerFrame';
+import NumbersFrame from '../../components/numbersFrame/NumbersFrame';
 
 class Game extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-5">
-                    <StarsFrame />
+            <div>
+                <div className="row">
+                    <div className="col-md-5">
+                        <StarsFrame />
+                    </div>
+                    <div className="col-md-2">
+                        <ButtonFrame />
+                    </div>
+                    <div className="col-md-5">
+                        <AnswerFrame />
+                    </div>
                 </div>
-                <div className="col-md-2">
-                    <ButtonFrame />
-                </div>
-                <div className="col-md-5">
-                    <AnswerFrame />
+                <div className="row">
+                    <div className="col-md-12">
+                        <NumbersFrame />
+                    </div>
                 </div>
             </div>
         );
