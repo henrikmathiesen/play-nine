96f0b369a31bf7798e1145d82c86194a3fd05b8a
Henrik <henrik_mathiesen@hotmail.com>
2016-07-23 18:15:38 +0200

    -design

--- src/components/starsFrame/StarsFrame.js
+++ src/components/starsFrame/StarsFrame.js
@@ -3,13 +3,16 @@ import './stars-frame.css';
 
 class StarsFrame extends Component {
     render() {
+        let numberOfStars = 5;
+        let stars = [];
+
+        for (var index = 0; index < numberOfStars; index++) {
+            stars.push(<span key={index} className="glyphicon glyphicon-star"></span>);
+        }
+
         return(
             <div id="stars-frame" className="well well-lg">
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
+                {stars}
             </div>
         );
     }
