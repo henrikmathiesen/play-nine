52c213494bb904c89ddd08093c90fe2925d311df
Henrik <henrik_mathiesen@hotmail.com>
2016-09-19 18:48:19 +0200

    -included fastclick

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './pages/Layout'
+import FastClick from 'fastclick'; 
+
+FastClick.attach(document.body);
 
 ReactDOM.render(
   <Layout />,
