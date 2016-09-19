484bf6c27b80d5c1c4dbbb38e0c24bd884332b13
Henrik <henrik_mathiesen@hotmail.com>
2016-09-19 19:21:48 +0200

    -leader board

---
+++ src/components/leaderBoardAlert/LeaderBoardAlert.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './leader-board-alert.css';
+
+class LeaderBoardAlert extends Component {
+
+    constructor() {
+        super();
+
+        this.handleCollapse = this.handleCollapse.bind(this);
+
+        this.state = {
+            isVisible: false
+        };
+    }
+
+    handleCollapse() {
+        this.setState({ isVisible: !this.state.isVisible });
+    }
+
+    render() {
+        let alertClasses = 'alert alert-warning alert-dismissible' + (!this.state.isVisible ? ' alert-collpase' : '');
+        let buttonClasses = this.state.isVisible ? 'glyphicon glyphicon-chevron-up' : 'glyphicon glyphicon-chevron-down';
+
+        return (
+            <div id="leader-board-alert" className={alertClasses}>
+                <button type="button" className="close" onClick={this.handleCollapse}><span className={buttonClasses}></span></button>
+                <div className="text-center">
+                    <h2 className="text-uppercase"><strong>Leader Board</strong></h2>
+                    <hr />
+                </div>
+                <div>
+                    <ul>
+                        <li>1&nbsp;&nbsp;&nbsp;Henry</li>
+                        <li>2&nbsp;&nbsp;&nbsp;Henry</li>
+                        <li>3&nbsp;&nbsp;&nbsp;Henry</li>
+                        <li>4&nbsp;&nbsp;&nbsp;Henry</li>
+                        <li>5&nbsp;&nbsp;&nbsp;Henry</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+
+}
+export default LeaderBoardAlert;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../components/header/Header';
 import RulesAlert from '../components/rulesAlert/RulesAlert';
+import LeaderBoardAlert from '../components/leaderBoardAlert/LeaderBoardAlert';
 import Game from './game/Game';
 import './layout.css';
 
@@ -9,7 +10,14 @@ class Layout extends Component {
     return (
       <div className="container">
         <Header />
-        <RulesAlert />
+        <div className="row">
+          <div className="col-md-9">
+            <RulesAlert />
+          </div>
+          <div className="col-md-3">
+            <LeaderBoardAlert />
+          </div>
+        </div>
         <Game />
       </div>
     );
